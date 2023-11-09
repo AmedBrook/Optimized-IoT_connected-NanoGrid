e0cae5f2e26a7736842a5465b7e1980c96acd113
Ahmed <mabrook.amed@gmail.com>
2023-11-09 22:04:29 +0200

    Update interface script

--- mqtt_ui/Novia_broker.py
+++ mqtt_ui/Novia_broker.py
@@ -2,29 +2,42 @@ import paho.mqtt.client as mqtt #importing the client
 import time #importing time 
 import access_key
 from datetime import datetime
+from queue import Queue
 
 # Getting the current date and time
-dt = datetime.now()
-
+q=Queue()
 ####
 
 username= access_key.username
 password= access_key.password
 
+sub_topic = 'open/meteoria/solarRadiation'
+meseaure = 'Radiation'
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-    client.subscribe("open/meteoria/solarRadiation")
+    client.subscribe(sub_topic)
 
 def on_message(client, userdata, message): 
-    print('message received', str(message.payload.decode('utf-8')),'kWh/m2','=======', dt, '=======')
+    print('#######', meseaure, ':',str(message.payload.decode('utf-8')),'kWh/m2','#######')
+    print('=======', str(datetime.now()), '=======')
     print('message topic is : ',message.topic)
     print('message qos is : ', message.qos)
     print('mesaage retain flag = ', message.retain)
+    q.put(message)
+
 
 def on_log(client, userdata, level, buf):
     print("log: ",buf)
+
+
+while not q.empty():
+   message = q.get()
+   if message is None:
+       continue
+   print("received from queue",str(message.payload.decode("utf-8")))
+
 
 ###
 
@@ -42,10 +55,28 @@ client.loop_stop() #stop the loop
 
 client.loop_start() #start the loop
 client.on_log=on_log #printing log information
-print("Subscribing to topic","open/meteoria/solarRadiation")
-client.subscribe("open/meteoria/solarRadiation")
+print("Subscribing to topic", sub_topic)
+client.subscribe(sub_topic)
+
+
+def publish(client):
+    msg = 0
+    pub_topic = 'meteoria/optimizer/gensetControl'
+    while True:
+        time.sleep(1)
+        result = client.publish(pub_topic, msg)
+        # result: [0, 1]
+        status = result[0]
+        if status == 0:
+            print(f"Send `{msg}` to topic `{pub_topic}`")
+        else:
+            print(f"Failed to send message to topic {pub_topic}")
 
 
 
+def run():
+    publish(client)
 
 
+if __name__ == '__main__':
+    run()
